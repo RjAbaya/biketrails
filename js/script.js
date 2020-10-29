a78c4463c37250e3f0d54fae6496f2edd911f805
unknown <rjabaya0110@gmail.com>
2020-10-29 10:01:36 -0400

    Modification in JS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
-function submitEvent (event) {
-    event.preventDefault()
+const submitBtn = document.getElementById('submitbtn');
 
-    const name = document.getElementById('name').value
-    console.log(name)
-    const mail = document.getElementById('mail').value
-    console.log(mail)
-    const message = document.getElementById('message').value
-    console.log(message)
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value.indexOf('@');
+    const message = document.getElementById('message').value;
+    const form = document.getElementById('myForm');
+
+    const errorName = document.getElementById('errorName');
+    const errorEmail = document.getElementById('errorEmail');
+    const errorMessage = document.getElementById('errorMessage');
+
+form.addEventListener('submit', (e) => 
+{
     
-    // output.innerText = `Name = ${name} Email = ${mail} Message = ${message}`
-}
+    let text = []
 
-const output = document.getElementById('output')
-const form = document.getElementById('myForm')
+    if ( name === "" )
+    {
+        text.push('Provide a valid name')
+        errorName.innerText = text;
+    }
 
-form.addEventListener('submit', submitEvent)
+    if ( email === "" || email == -1 )
+    {
+        text.push('Email must be valid')
+        errorEmail.innerText = text;
+    }
+
+    if (message === "" || message.length <= 10)
+    {
+        text.push('Tell me more!')
+        errorMessage.innerText = text;
+    }
+
+        
+    if (text.length > 0 ) {
+        e.preventDefault();
+    }
+             
+
+})
+
+
+
+
+
+   
+
+
+
