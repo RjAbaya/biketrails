a4388c1d8e25c51334cf244a985788718c93345b
unknown <rjabaya0110@gmail.com>
2020-11-10 18:49:15 -0500

    Add contacts.js on the website

---
+++ js/contacts.js
@@ -0,0 +1,20 @@
+// This is a function of a form in the contact.html
+
+const email = document.getElementById('email')
+const messageSent = document.getElementById("messageSent")
+const form = document.getElementById('myForm')
+
+    email.addEventListener('input', function (event) {
+        if ( email.validity.valueMissing || email.validity.patternMismatch ) {    // Checking the email condition
+            email.setCustomValidity('Please enter a valid email account in the format of email@domain.com')
+        } else {
+            email.setCumtomValidity('')
+        }
+    })
+
+    
+    function mySubmitFunction(event) {
+    event.preventDefault()
+    messageSent.innertext = "I will get in touch with your email " + email.value + "and thank you!"
+    form.style.display = "none"
+}
--- js/script.js
+++ js/script.js
@@ -1,73 +1,6 @@
-function formSend() {
-    //alert("TEsting");
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    const errorFound = false;
-
-    if (name == "") // Checking the name condition
-    {
-        //Leaving the name area blank will be an error
-
-        var el = document.getElementById("errorName");
-        el.classList.remove("hidden");
-        errorFound = true;
-    }
-    else{
-        var el = document.getElementById("errorName");
-        el.classList.add("hidden");
-    }
-
-    if ( email === "" || email.indexOf("@") ==-1 ) // Checking the email condition
-    {
-        // Leaving the email area will be an error
-        // No '@' in the email will be an error
-
-        var el = document.getElementById("errorEmail");
-        el.classList.remove("hidden");
-        errorFound = true;
-    }
-    else{
-
-        var el = document.getElementById("errorEmail");
-        el.classList.add("hidden");
-        
-    }
-
-    if ( message === "" || message.length <= 10 ) // Checking the message condition
-    {
-        // Less than or equal to 10 characters will be an error
-
-        var el = document.getElementById("errorMessage");
-        el.classList.remove("hidden");
-        errorFound = true;
-
-    }
-    else
-    {
-        var el = document.getElementById("errorMessage");
-        el.classList.add("hidden");
-    }
-
-    if(errorFound == true) // If error found an alert will pop out
-    {
-        alert("ErrorFound");    
-    }
-    else
-    {
-        // If condition is successful, it will be console.
-
-        console.log(name);
-        console.log(email);
-        console.log(message);
-        console.log("Form Submitted");
-    }
-    
-}// End of the form validation
 
 
+// This is a function of picture slideshow on the index.html
 
 var slideIndex = 1;
 showDivs(slideIndex);
@@ -84,7 +17,7 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block"; 
 }
 
 
